3b63f6313c7365d7dc8a7cdd7b48f477b8f111f3
Alvina <alvinadr24@gmail.com>
2024-02-11 12:09:38 +0100

    feat: sequence system simplified, like disco elysium

--- Assets/Script/SequenceData.cs
+++ Assets/Script/SequenceData.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(fileName = "Sequence", menuName = "ScriptableObjects/Sequence", order = 1)]
 public class SequenceData : ScriptableObject
 {
-    public AudioClip audioClip;
-    public List<TextLine> textLineList = new List<TextLine>(); 
-
-    [System.Serializable]
-    public class TextLine
+    public enum SequenceType
     {
-        public string text;
-        public float timer;
-        public float length;
+        Narration = 0,
+        Choice = 1
     }
+    public SequenceType sequenceType;
+    public FMODUnity.EventReference audioEvent;
+    [TextArea(4, 10)]
+    public string text;
+    [ShowIf("sequenceType", SequenceType.Choice)]
+    [HorizontalGroup]
+    public List<SequenceData> leftChoice;
+    [ShowIf("sequenceType", SequenceType.Choice)]
+    [HorizontalGroup]
+    public List<SequenceData> rightChoice;
 }
--- Assets/Script/SequenceReader.cs
+++ Assets/Script/SequenceReader.cs
@@ -2,64 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using FMODUnity;
 
 public class SequenceReader : MonoBehaviour
 {
-    public float chrono;
     public int sequenceIndex = 0;
     public List<SequenceData> sequenceList = new List<SequenceData>(); 
-    public int lineIndex = 0;
     private bool stopReading = false;
-    [SerializeField] private AudioSource audioSource;
     [SerializeField] private TextMeshProUGUI textUI;
-
-    void Update()
-    {
-        if (stopReading) return;
-        chrono += Time.deltaTime;
-        if (sequenceList[sequenceIndex].textLineList[lineIndex].timer <= chrono)
-        {
-            ShowLine(sequenceList[sequenceIndex].textLineList[lineIndex]);
-        }
-    }
+    [SerializeField] private FMOD.Studio.EventInstance audioEvent;
 
     private void Start()
     {
-        audioSource.clip = sequenceList[sequenceIndex].audioClip;
-        audioSource.Play();
+        ShowSequence(sequenceList[sequenceIndex]);
     }
 
-    public void ShowLine(SequenceData.TextLine _textLine)
+    public void ShowSequence(SequenceData _sequence)
     {
-        Debug.Log(_textLine.text);
-        textUI.text = _textLine.text;
-        StartCoroutine(HideLine(_textLine));
-        lineIndex++;
-        if (lineIndex >= sequenceList[sequenceIndex].textLineList.Count)
+        Debug.Log(_sequence.text);
+        audioEvent = RuntimeManager.CreateInstance("event:/1n");
+        textUI.text = _sequence.text;
+        if (_sequence.sequenceType == SequenceData.SequenceType.Choice)
         {
-            NextSequence();
+            ShowChoice(_sequence);
         }
+    }
+
+    public void ShowChoice(SequenceData _sequence)
+    {
+        //get buttons, change value of button depending on which button is clicked, etc
     }
 
     public void NextSequence()
     {
-        lineIndex = 0;
         sequenceIndex++;
-        chrono = 0;
         if (sequenceIndex >= sequenceList.Count) //if last sequence
         {
-            stopReading = true;
+            Debug.Log("ENDING");
         } else
         {
-            audioSource.clip = sequenceList[sequenceIndex].audioClip;
-            audioSource.Play();
+            ShowSequence(sequenceList[sequenceIndex]);
         }
-    }
-
-    IEnumerator HideLine(SequenceData.TextLine _textLine)
-    {
-        yield return new WaitForSeconds(_textLine.length);
-        Debug.Log("HIDE LINE");
-        if (textUI.text == _textLine.text) textUI.text = "";
     }
 }
